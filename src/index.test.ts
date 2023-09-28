f678319b2086b7f40fa6d96eb4f15de005dd78d2
Stadly <magnar@myrtveit.com>
2023-09-28 18:25:30 +0000

    Set up @testing-library/svelte

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,20 @@
 import { describe, it, expect } from 'vitest';
+import { render } from "@testing-library/svelte";
+import Page from "./routes/+page.svelte";
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
+// Uncomment this to clean up after each test, as it is not done automatically.
+// import { cleanup } from "@testing-library/svelte";
+// import { afterEach } from "vitest";
+// afterEach(() => {
+//   cleanup();
+// });
+
+it('should render page', () => {
+	const { getByRole } = render(Page);
+	expect(getByRole("heading", { level: 1 }).textContent).toBe("Welcome to SvelteKit");
+});
+
+it('should have been cleaned up before next test is run', () => {
+	const { getByRole } = render(Page);
+	expect(getByRole("heading", { level: 1 }).textContent).toBe("Welcome to SvelteKit");
 });
