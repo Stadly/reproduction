c37bd8d876c3e41444a5a68946f4e00e5904820d
Stadly <magnar@myrtveit.com>
2023-09-28 17:06:13 +0000

    Set up vitest-axe

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,6 @@
 import { describe, it, expect } from 'vitest';
+import { axe } from "vitest-axe";
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
+it('has no violations', async () => {
+	expect(await axe("<main>test</main>")).toHaveNoViolations();
 });
