03d91357eb88f5d6f92e243f88c8679ce9e935ec
Magnar Ovedal Myrtveit <magnar@myrtveit.com>
2024-11-25 11:17:46 +0000

    Cannot import @auth/sveltekit in Vitest

---
+++ src/app.test.ts
@@ -0,0 +1,6 @@
+import { handle } from "./auth.js"; // <-- This fails!
+import { it } from "vitest";
+
+it("test the handle hook", () => {
+    
+});
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
+import { svelteTesting } from '@testing-library/svelte/vite'
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), svelteTesting()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts'],
+  },
 })
